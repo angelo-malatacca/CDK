1f89a86943e994a93563384592fcc948acf18575
Angelo Malatacca <angelo.malatacca@sentia.com>
2019-12-02 15:03:20 +0100

    Simple CDK infrastructure

--- my_cdk_project/my_vpc_stack.py
+++ my_cdk_project/my_vpc_stack.py
@@ -11,7 +11,7 @@ class MyVPCStack(core.Stack):
         self.vpc = ec2.Vpc(
             self, 'MyVpc',
             cidr='10.0.0.0/16',
-            max_azs=2, # use all available AZs,
+            max_azs=99, # use all available AZs,
             subnet_configuration=[ 
                # { 'cidrMask': 28, 'name': 'nat', 'subnetType': ec2.SubnetType.PUBLIC },
                 { 'cidrMask': 24, 'name': 'public-subnet', 'subnetType': ec2.SubnetType.PUBLIC },
